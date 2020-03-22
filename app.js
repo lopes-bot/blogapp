ebf179ce4b2858bac64f9ffc9135584ff8cf84ae
unknown <andrelopessfla@gmail.com>
2020-03-21 23:20:56 -0300

    projeto inicial

---
+++ app.js
@@ -0,0 +1,30 @@
+//carregando modulos
+    const express = require('express')
+    const handlebars = require('express-handlebars')
+    const bodyParser = require('body-parser')
+    const app = express();
+    const admin = require('./routes/admin')
+    const path = require("path")
+    //const mongoose = require('mongoose')
+
+//configurações
+    //body parser
+        app.use(bodyParser.urlencoded({extended: true}))
+        app.use(bodyParser.json())
+    //handlebars
+        app.engine('handlebars',handlebars({defaultLayout:'main'}))
+        app.set('views engine','handlebars');
+    //mongoose
+
+    //public
+        app.use(express.static(path.join(__dirname,"public")))
+       // app.use(express.static('public'));
+
+//rotas
+    app.use('/admin',admin)
+//outros
+const PORT = 8081
+
+app.listen(PORT,()=>{
+    console.log("servidor rodando!");
+})
---
+++ routes/admin.js
@@ -0,0 +1,18 @@
+const express= require("express")
+const router = express.Router()
+
+router.get('/',(req,res)=>{
+   //da erro quando eu chamo res.render("admin/index")
+   res.render("admin/index");
+
+})
+router.get('/posts',(req,res)=>{
+    res.send("Página de Posts")
+})
+
+router.get('/categorias',(req,res)=>{
+    res.send("Páginas de categorias")
+})
+
+
+module.exports = router
