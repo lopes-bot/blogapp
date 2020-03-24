ef999c642303fa445c8b4b2a64828865e82e736d
unknown <andrelopessfla@gmail.com>
2020-03-23 22:26:49 -0300

    pagina de postagem finalizada

--- routes/admin.js
+++ routes/admin.js
@@ -165,5 +165,60 @@ router.post("/postagens/nova",(req,res)=>{
     }
 
 })
+router.get("/postagens/edit/:id",(req,res)=>{
+    Postagem.findOne({_id:req.params.id}).then((postagem)=>{
+
+        Categoria.find().then((categorias)=>{
+
+            res.render("../views/admin/editpostagens.handlebars",{categorias: categorias, postagem: postagem})
+
+        }).catch((err)=>{
+
+            req.flash("error_msg","Erro ao carregar as categorias!")
+             res.redirect("/admin/postagens")
+        })
+
+    }).catch((err)=>{
+        req.flash("error_msg","Erro ao carregar o formulario de edição!")
+        res.redirect("/admin/postagens")
+    })
+    
+
+})
+
+router.post("/postagens/edit",(req,res)=>{
+    Postagem.findOne({_id: req.body.id}).then((postagem)=>{
+        postagem.titulo= req.body.titulo
+        postagem.slug = req.body.slug
+        postagem.descricao = req.body.descricao
+        postagem.conteudo = req.body.conteudo
+        postagem.categoria = req.body.categoria
+
+        postagem.save().then(()=>{
+            req.flash("success_msg","Postagem editada com sucesso!")
+            res.redirect("/admin/postagens")
+        }).catch((err)=>{
+            console.log("esse e o erro->"+err)
+            req.flash("error_msg","erro interno!")
+            res.redirect("/admin/postagens")
+
+        })
+
+
+    }).catch((err)=>{
+        req.flash("error_msg","Erro ao salvar a edição!")
+        res.redirect("/admin/postagens")
+    })
+})
+//o uso do get nao e uma forma segura da deletar
+router.get("/postagens/delete/:id",(req,res)=>{
+    Postagem.remove({_id: req.params.id}).then(()=>{
+        req.flash("success_msg","Postagem deletada com sucesso!")
+        res.redirect("/admin/postagens")
+    }).catch((err)=>{
+        req.flash("error_msg","Erro interno")
+        res.redirect("/admin/postagens")
+    })
+})
 
 module.exports = router
